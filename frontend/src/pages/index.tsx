059939d5abcd348d25028ce93a9f994a2e1e78b6
Gina <ginagrosso01@gmail.com>
2025-04-17 13:01:01 -0300

    Proyecto base (sin node_modules)

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-700">
+        ¡Tailwind funciona! 🌸
+      </h1>
+    </div>
+  );
+}
